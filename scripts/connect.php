6bf9c0dce1b65164d001f6b6067a9ebcdceb4154
malinuuus <maliny.brzoskwiniowe@gmail.com>
2023-03-15 13:04:39 +0100

    Add connect_erno to connect.php file

--- scripts/connect.php
+++ scripts/connect.php
@@ -2,5 +2,5 @@
   // połączenie z bazą danych
   // new mysqli(hostname, username, password, database)
   $conn = new mysqli("localhost", "root", "", "wsb_pai_dlizn_k15");
-
+  // echo $conn->connect_errno; // komunikat o połączeniu, 0 - dobrze
 ?>
