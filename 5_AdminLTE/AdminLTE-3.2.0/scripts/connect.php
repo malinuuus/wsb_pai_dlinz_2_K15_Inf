07168af875a573b17842327ead658230f8a2a3b8
malinuuus <maliny.brzoskwiniowe@gmail.com>
2023-04-12 13:40:31 +0200

    Modify register file

---
+++ 5_AdminLTE/AdminLTE-3.2.0/scripts/connect.php
@@ -0,0 +1,5 @@
+<?php
+  // połączenie z bazą danych
+  // new mysqli(hostname, username, password, database)
+  $conn = new mysqli("localhost", "root", "", "wsb_pai_dlinz_k15_register");
+  // echo $conn->connect_errno; // komunikat o połączeniu, 0 - dobrze
---
+++ 5_AdminLTE/AdminLTE-3.2.0/scripts/register.php
@@ -0,0 +1,12 @@
+<?php
+//echo "<pre>";
+//print_r($_POST);
+//echo "</pre>";
+
+require_once "connect.php";
+// zabezpieczenie przed sql injection
+$stmt = $conn->prepare("INSERT INTO users (email, city_id, firstName, lastName, birthday, password, created_at) VALUES (?, ?, ? ,?, ?, ?, CURRENT_TIMESTAMP());");
+$stmt->bind_param("sissss", $_POST["email1"], $_POST["city_id"], $_POST["firstName"], $_POST["lastName"], $_POST["birthday"], $_POST["pass1"]);
+$stmt->execute();
+
+echo $stmt->affected_rows;
