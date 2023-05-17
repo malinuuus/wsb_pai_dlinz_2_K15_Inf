b5fc3511a081ff9cefe6f08d98835faeccba86b1
malinuuus <maliny.brzoskwiniowe@gmail.com>
2023-05-17 13:57:01 +0200

    Add input sanitization and an array of errors

--- 5_AdminLTE/scripts/register.php
+++ 5_AdminLTE/scripts/register.php
@@ -1,72 +1,103 @@
 <?php
-//echo "<pre>";
-//print_r($_POST);
-//echo "</pre>";
-session_start();
-$error = 0;
+// sanityzacja
+// poszukać czemu trim nie działa
+function sanitizeInput(&$input) {
+    $input = htmlentities(stripslashes(trim($input)));
+    return $input;
+}
+//
+//echo $_POST["firstName"]." ==> ".sanitizeInput($_POST["firstName"]).", ilość znaków: ".strlen($_POST["firstName"]);
+//exit();
 
-foreach ($_POST as $key => $value) {
-    if (empty($value)) {
-        $_SESSION["error"] = "Wypełnij wszystkie pola w formularzu!";
-        $error++;
-        break;
+// działa też, jeśli ktoś próbuje zmienić method na post w dev toolsach
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // tablica z nazwami wymaganych pól
+    $required_fields = ["firstName", "lastName", "email1", "email2", "pass1", "pass2", "birthday", "city_id", "gender"];
+    session_start();
+    $error = 0;
+    $errors = [];
+
+    foreach ($required_fields as $value) {
+        if (empty($_POST[$value])) {
+            $errors[] = "Pole <b>$value</b> jest wymagane!";
+            $error++;
+        }
     }
-}
 
-if ($error == 0 && $_POST["email1"] !== $_POST["email2"]) {
-    $_SESSION["error"] = "Adres email musi być taki sam!";
-    $error++;
-}
+    if (!empty($errors)) {
+        $_SESSION["error"] = implode("<br>", $errors);
+        echo "<script>history.back();</script>";
+        exit();
+    }
 
-if ($error == 0 && $_POST["pass1"] !== $_POST["pass2"]) {
-    $_SESSION["error"] = "Hasło musi być takie samo w obu polach!";
-    $error++;
-}
+    if ($_POST["email1"] !== $_POST["email2"])
+        $errors[] = "Adres email musi być taki sam!";
 
-if ($error == 0 && !preg_match('/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\w\d\s])\S{8,}$/', $_POST["pass1"])) {
-    $_SESSION["error"] = "Hasło nie spełnia wymagań!";
-    $error++;
-}
+    if ($_POST["additional_email1"] !== $_POST["additional_email2"]) {
+        $errors[] = "Adres email musi być taki sam!";
+    } else {
+        if (empty($_POST["additional_email1"]))
+            $_POST["additional_email1"] = null;
+    }
 
-if ($error == 0 && !isset($_POST["terms"])) {
-    $_SESSION["error"] = "Zatwierdź regulamin!";
-    $error++;
-}
+    if ($_POST["pass1"] !== $_POST["pass2"])
+        $errors[] = "Hasło musi być takie samo w obu polach!";
 
-if ($error == 0 && !isset($_POST["gender"])) {
-    $_SESSION["error"] = "Zaznacz płeć!";
-    $error++;
-}
+    if (!isset($_POST["gender"]))
+        $errors[] = "Zaznacz płeć!";
 
-// sprawdzenie, czy email już istnieje
-require_once "connect.php";
-if ($error == 0) {
-    $stmt = $conn->prepare("SELECT * FROM users WHERE email = ?");
-    $stmt->bind_param("s", $_POST["email1"]);
+    if (!isset($_POST["terms"]))
+        $errors[] = "Zatwierdź regulamin!";
+
+    if (!empty($errors)) {
+        $_SESSION["error"] = implode("<br>", $errors);
+        echo "<script>history.back();</script>";
+        exit();
+    }
+
+    if (!preg_match('/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\w\d\s])\S{8,}$/', $_POST["pass1"])) {
+        $_SESSION["error"] = "Hasło nie spełnia wymagań!";
+        echo "<script>history.back();</script>";
+        exit();
+    }
+
+    // sprawdzenie, czy email już istnieje
+    require_once "connect.php";
+    if ($error == 0) {
+        $stmt = $conn->prepare("SELECT * FROM users WHERE email = ?");
+        $stmt->bind_param("s", $_POST["email1"]);
+        $stmt->execute();
+
+        if ($stmt->get_result()->num_rows > 0) {
+            $errors = "Podany e-mail jest zajęty!";
+            $error++;
+        }
+    }
+
+    if ($error != 0) {
+        echo "<script>history.back()</script>";
+        exit();
+    }
+
+    foreach ($_POST as $key => $value) {
+        if (!$_POST["pass1"] && !$_POST["pass2"]) {
+            sanitizeInput($_POST["$key"]);
+        }
+    }
+
+    $hashedPassword = password_hash($_POST["pass1"], PASSWORD_ARGON2I);
+    $avatarPath = $_POST["gender"] == "w" ? "./img/woman-avatar.jpg" : "./img/man-avatar.jpg";
+
+    // zabezpieczenie przed sql injection
+    $stmt = $conn->prepare("INSERT INTO users (email, additional_email, city_id, firstName, lastName, birthday, gender, avatar, password, created_at) VALUES (?, ?, ?, ? ,?, ?, ?, ?, ?, CURRENT_TIMESTAMP());");
+    $stmt->bind_param("ssissssss", $_POST["email1"], $_POST["additional_email1"], $_POST["city_id"], $_POST["firstName"], $_POST["lastName"], $_POST["birthday"], $_POST["gender"], $avatarPath, $hashedPassword);
     $stmt->execute();
 
-    if ($stmt->get_result()->num_rows > 0) {
-        $_SESSION["error"] = "Podany e-mail jest zajęty!";
-        $error++;
+    if ($stmt->affected_rows == 1) {
+        $_SESSION["success"] = "Zarejestrowano użytkownika";
+    } else {
+        $errors[] = "Nie udało się zarejestrować użytkownika!";
     }
 }
 
-if ($error != 0) {
-    echo "<script>history.back()</script>";
-    exit();
-}
-
-$hashedPassword = password_hash($_POST["pass1"], PASSWORD_ARGON2I);
-$avatarPath = $_POST["gender"] == "w" ? "./img/woman-avatar.jpg" : "./img/man-avatar.jpg";
-
-// zabezpieczenie przed sql injection
-$stmt = $conn->prepare("INSERT INTO users (email, city_id, firstName, lastName, birthday, gender, avatar, password, created_at) VALUES (?, ?, ? ,?, ?, ?, ?, ?, CURRENT_TIMESTAMP());");
-$stmt->bind_param("sissssss", $_POST["email1"], $_POST["city_id"], $_POST["firstName"], $_POST["lastName"], $_POST["birthday"], $_POST["gender"], $avatarPath, $hashedPassword);
-$stmt->execute();
-
-if ($stmt->affected_rows == 1) {
-    $_SESSION["success"] = "Zarejestrowano użytkownika";
-} else {
-    $_SESSION["error"] = "Nie udało się zarejestrować użytkownika!";
-}
 header("location: ../pages/register.php");
